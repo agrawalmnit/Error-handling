10b67707a1b7f553f67b9d6e5391989f1a225a8d
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2023-01-02 15:50:26 +0530

    Add files via upload

---
+++ ConsoleApp48/ConsoleApp48/CalculationException.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp48;
+
+public class CalculationException : Exception
+{
+    private const string DefaultMessage = "An error occurred during calculation. Ensure that the operator is supported and that the values are within the valid ranges for the requested operation.";
+
+    
+    public CalculationException() : base(DefaultMessage) { }
+
+    public CalculationException(string message) : base(message) { }
+
+    
+    public CalculationException(Exception innerException) :
+        base(DefaultMessage, innerException)
+    { }
+
+    
+    public CalculationException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
---
+++ ConsoleApp48/ConsoleApp48/Calculator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace ConsoleApp48;
+
+public class Calculator
+{
+    public int Calculate(int number1, int number2, string operation)
+    {
+        string nonNullOperation = 
+            operation ?? throw new ArgumentNullException(nameof(operation));
+
+        if (nonNullOperation == "/")
+        {
+            try
+            {
+                return Divide(number1, number2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("...logging...");
+               
+
+                throw new CalculationException(ex);
+            }
+        }
+        
+
+        else if (nonNullOperation == "-")
+        {
+            try
+            {
+                return Substract(number1, number2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("...logging...");
+                
+
+                throw new CalculationException(ex);
+            }
+        }
+       
+
+         else if (nonNullOperation == "+")
+        {
+            try
+            {
+                return Add(number1, number2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("...logging...");
+                
+
+                throw new CalculationException(ex);
+            }
+        }
+        
+
+        else if (nonNullOperation == "*")
+        {
+            try
+            {
+                return Multiply(number1, number2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("...logging...");
+                
+                throw new CalculationException(ex);
+            }
+        }
+        else
+        {
+            throw new CalculationOperationNotSupportedException(operation);
+        }
+
+
+    }
+
+
+
+    private int Divide(int number, int divisor) => number / divisor;
+
+    private int Add(int number1, int number2) => number1 + number2;
+
+    private int Substract(int substractor, int substracting) => substractor - substracting;
+
+    private int Multiply(int number1, int number2) => (number1 * number2);
+}
+
